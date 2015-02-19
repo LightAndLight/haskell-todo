c9cb0241b04be0d53364a3ad6325871d35dbe678
LightAndLight <isaace71295@gmail.com>
2015-02-19 20:38:58 +1000

    Refactored to no longer use StateT, moved code
    Moved load and save functions from Main.Model to Main.Controller
    
    Functions now pass around the ProgramData type instead of using StateT

--- Main/Controller.hs
+++ Main/Controller.hs
@@ -1,56 +1,85 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main.Controller(requestChoice,parseChoice) where
+module Main.Controller(runCommand) where
 
 import Main.Model
-import Prelude hiding (getLine,putStrLn,words,unwords,takeWhile,drop,length)
+import Control.Applicative ((<$>),(<*>))
 import Control.Monad.State.Lazy
-import Data.Text (Text,split,toLower,append,words,unwords,takeWhile,drop,length)
-import Data.Text.IO (getLine,putStrLn)
+import Data.Aeson
+import Data.ByteString.Lazy as B (ByteString, readFile, writeFile)
+import Data.Text (pack)
 import Data.Text.Read (decimal)
 import System.Exit (exitSuccess)
 
-requestChoice :: ProgramState Text
-requestChoice = StateT $ \ps -> do 
-	putStrLn ("Enter command") 
-	line <- getLine
-	return (line,ps)
+instance FromJSON TodoItem where
+	parseJSON (Object v) = TodoItem <$>
+		v .: "date" <*>
+		v .: "message"
+	parseJSON _ = mzero
 
-parseChoice :: Text -> ProgramState ()
-parseChoice choice =
-    let (command,arguments) = splitAtFirst ' ' choice
+instance ToJSON TodoItem where
+	toJSON (TodoItem date message) = object ["date" .= date,
+						"message" .= message]
+
+runCommand :: String -> ProgramData -> IO ProgramData
+runCommand cmd pd =
+    let (command,arguments) = splitAtFirst ' ' cmd
         (arg1,rest) = splitAtFirst ' ' arguments
+        parg1 = pack arg1
+        prest = pack rest
     in
     case command of
-        "add"       -> addItem $ TodoItem arg1 rest
-        "remove"    -> removeItem . parseInt $ arg1
-        "save"      -> saveList arg1
-        "load"      -> loadList arg1
-        "quit"      -> quit 
-        _ -> invalidChoice command
+        "add"       -> return $ addItem (TodoItem parg1 prest) pd
+        "remove"    -> return $ removeItem (parseInt arg1) pd
+        "save"      -> saveList arg1 pd
+        "load"      -> loadList arg1 pd
+        "quit"      -> quit  pd
+        _           -> return $ invalidChoice command pd
 
-invalidChoice :: Text -> ProgramState ()
-invalidChoice cmd = StateT $ \ps -> do
-		putStrLn $ append cmd " is an invalid command."
-		return ((),ps)
+invalidChoice :: String -> ProgramData -> ProgramData
+invalidChoice cmd pd = pd { errorMsg = Just (cmd ++ "is an invalid command") }
 
-quit :: ProgramState ()
-quit = StateT $ \(ProgramData s l fp) -> if s
+loadList :: String -> ProgramData -> IO ProgramData
+loadList fp pd = do
+    contents <- B.readFile fp
+    return $ case (decode contents :: Maybe TodoList) of
+        Just list   -> ProgramData True list fp Nothing
+        Nothing     -> pd { errorMsg = Just "Parse error in JSON file" }
+
+saveList :: String -> ProgramData -> IO ProgramData
+saveList "" pd = do
+    B.writeFile (filepath pd) . encode $ list pd
+    return $ pd { 
+        saved = True
+        , errorMsg = Nothing
+    }
+
+saveList fp pd = do 
+    B.writeFile fp . encode $ list pd
+    return $ pd { 
+        saved = True 
+        , filepath = fp
+        , errorMsg = Nothing
+    }
+
+quit :: ProgramData -> IO ProgramData
+quit pd = if saved pd
     then exitSuccess
     else do
         putStrLn "Current list is unsaved. Quit without saving? (y/n)"
         choice <- getLine
         case choice of
             "y" -> exitSuccess
-            "n" -> return ((),ProgramData s l fp)
-            _   -> runStateT quit $ ProgramData s l fp
+            "n" -> return pd
+            _   -> quit pd
 
-parseInt :: Text -> Maybe Int
-parseInt tx = case decimal tx of
-	Left _ -> Nothing
-	Right (n,xs) -> if xs == "" then Just n else Nothing
+parseInt :: String -> Int
+parseInt tx = case decimal ptx of
+	Left _ -> -1
+	Right (n,xs) -> if xs == "" then n else -1
+    where ptx = pack tx
 
-splitAtFirst :: Char -> Text -> (Text,Text)
+splitAtFirst :: Char -> String -> (String,String)
 splitAtFirst c xs = (first,rest)
     where first = takeWhile (/=c) xs
           rest = drop (length first + 1) xs
