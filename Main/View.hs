69017c9fecc4316e8426b6f59ff17b839f0d9aac
LightAndLight <isaace71295@gmail.com>
2015-02-19 14:52:12 +1000

    Moved code, added show instance, changed printList
    Moved TodoItem show instance from Main.Model to Main.View
    
    Added a show instance for ProgramData to facilitate simplifying of the
    print function
    
    Updated printList to reflect the simplificatons

--- Main/View.hs
+++ Main/View.hs
@@ -3,6 +3,18 @@ module Main.View(printList) where
 import Main.Model
 import Control.Monad.State.Lazy
 import System.IO
+import Data.Text(unpack)
 
-printList :: ProgramState
-printList = StateT $ \(ProgramData b l) -> mapM_ (putStrLn . show) l >> return ((),ProgramData b l)
+instance Show ProgramData where
+    show (ProgramData s l fp) = 
+        "\nFile: " ++ fp ++ (if s then "" else "*") ++ "\n\n"
+        ++ (foldl (\acc x -> acc ++ show x) "" l)
+
+instance Show TodoItem where
+	show (TodoItem d m) = "Date: " ++ unpack d 
+		++ "\t\t\tMessage: " ++ unpack m ++ "\n"
+
+printList :: ProgramState ()
+printList = StateT $ \ps -> do
+    print ps
+    return ((),ps) 
