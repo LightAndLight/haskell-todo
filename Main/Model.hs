70a85dd837bfe22c3e23632f9ffb2da7d19ba9e1
LightAndLight <isaace71295@gmail.com>
2015-02-19 20:39:53 +1000

    Refactored to no longer use StateT
    Moved load and save from Main.Model to Main.Controller
    
    All functions of the in Main.Model are now pure

--- Main/Model.hs
+++ Main/Model.hs
@@ -1,72 +1,43 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main.Model(
     ProgramData(..)
-    , ProgramState(..)
 	, TodoList(..)
 	, TodoItem(..)
-	, loadList
-	, saveList
 	, addItem
 	, removeItem
 	) where
 
-import Control.Applicative ((<$>),(<*>))
-import Control.Monad (mzero)
-import Control.Monad.Trans (lift)
-import Control.Monad.Trans.Maybe
-import Control.Monad.State.Lazy
-import Data.Aeson
-import qualified Data.ByteString.Lazy as B
-import Data.Text (Text,unpack)
-import qualified System.IO as S
-
-type TodoList = [TodoItem]
+import Data.Text (Text)
 
 data TodoItem = TodoItem { 
 	date :: Text
 	, message :: Text
 	}
 
-data ProgramData = ProgramData Bool TodoList String
+type TodoList = [TodoItem]
 
-type ProgramState a = StateT ProgramData IO a 
+data ProgramData = ProgramData {
+    saved :: Bool 
+    , list :: TodoList 
+    , filepath :: String
+    , errorMsg :: Maybe String
+}
 
-instance FromJSON TodoItem where
-	parseJSON (Object v) = TodoItem <$>
-		v .: "date" <*>
-		v .: "message"
-	parseJSON _ = mzero
+addItem :: TodoItem -> ProgramData -> ProgramData
+addItem tdi pd = pd { 
+    saved = False
+    , list = tdi:(list pd)
+    , errorMsg = Nothing
+}
 
-instance ToJSON TodoItem where
-	toJSON (TodoItem date message) = object ["date" .= date,
-						"message" .= message]
-
-loadList :: Text -> ProgramState ()
-loadList fp = StateT $ \ps -> do
-		contents <- B.readFile . unpack $ fp
-		case (decode contents :: Maybe TodoList) of
-			Just list -> return ((),ProgramData True list (unpack fp))
-			Nothing -> S.putStrLn "Syntax Error in JSON file" >> return ((),ps)
-
-saveList :: Text -> ProgramState ()
-saveList "" = StateT $ \(ProgramData _ l fp) -> do
-		B.writeFile fp . encode $ l
-		return ((),ProgramData True l fp)
-
-saveList fp = StateT $ \(ProgramData _ l _) -> do
-		B.writeFile fps . encode $ l
-		return ((),ProgramData True l fps)
-        where fps = unpack fp 
-
-addItem :: TodoItem -> ProgramState ()
-addItem x = StateT $ \(ProgramData _ l fp) -> return ((),ProgramData False (x:l) fp)
-
-removeItem :: Maybe Int -> ProgramState ()
-removeItem Nothing  = lift $ putStrLn "Invalid index entered"
-removeItem (Just n) = StateT $ \(ProgramData _ l fp) -> return ((),ProgramData False (removeAt n l) fp)
+removeItem :: Int -> ProgramData -> ProgramData
+removeItem n pd 
+    | n < 0 = pd { errorMsg = Just "Invalid index" }
+    | otherwise = pd { 
+        saved = False 
+        , list = removeAt n (list pd)
+        , errorMsg = Nothing
+    }
 
 removeAt :: Int -> [a] -> [a]
 removeAt _ [] = []
 removeAt n xs = (take n xs) ++ (drop (n + 1) xs)
-
