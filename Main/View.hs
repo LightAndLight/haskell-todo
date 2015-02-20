7c00d5b41cc0f4666712bae7ce65cbac29576966
LightAndLight <isaace71295@gmail.com>
2015-02-20 15:53:09 +1000

    Updated show instance

--- Main/View.hs
+++ Main/View.hs
@@ -4,14 +4,11 @@ import Main.Model
 import System.IO
 import Data.Text(unpack)
 
-instance Show ProgramData where
-    show pd =   
-        case pd of
-            ProgramData s l fp Nothing -> "\nFile: " ++ fp 
-                ++ (if s then "" else "*") ++ "\n\n"
-                ++ (foldl (\acc x -> acc ++ show x) "" l)
-
-            ProgramData _ _ _ (Just error) -> "ERROR: " ++ error
+instance Show ProgramState where
+    show ps = "\nFile: " ++ (filepath ps) 
+        ++ (if saved ps then "" else "*") ++ "\n\n"
+        ++ printTodos ps
+        where printTodos ps = foldl (\acc x -> acc ++ show x) "" $ list ps
 
 instance Show TodoItem where
 	show (TodoItem d m) = "Date: " ++ unpack d 
